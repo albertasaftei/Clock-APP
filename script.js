cb9359648ce2b5048d6151074859d95b6569a37f
Albert Asaftei <albert.asaftei00@gmail.com>
2021-01-10 17:29:51 +0100

    code cleaning

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const timezoneAPI = "https://timezoneapi.io/api/ip/?token=aNsiQLfUDzdnhRxsLmJP";
-const quotesAPI = "https://quotes.rest/yaml/theysaidso.quotes.openapi.yaml?v1.1/qod"
+const quotesAPI = "https://api.paperquotes.com/apiv1/quotes/?tags=love,motivation&order=-likes"
 const time = document.getElementById("time");
 const timezone = document.getElementById("timezone");
 const position = document.getElementById("position");
 const greetings = document.getElementById("greetings");
-let url = 
 
 fetch(timezoneAPI).then((response) => {
     return response.json();
 }).then((data) => {
     position.innerText = `in ${data.data.timezone.capital}, ${data.data.timezone.country_code}`
+    timezone.innerText = `${data.data.datetime.offset_tzab}`
 }).catch((e) => {
     console.error(e);
 });
@@ -44,16 +44,3 @@ fetch(quotesAPI).then(response => {
 
     setTimeout(showDate, 1000);
 })();
-
-
-/* setInterval(function showDate() {
-    const date = new Date();
-    console.log(date.getHours(), date.getMinutes(), date.getSeconds())
-    if (date.getHours() > 12 && date.getHours() < 18) {
-        greetings.innerText = "Good afternoon, it's currently"
-    } else if (date.getHours() > 18 && date.getHours()) {
-        greetings.innerText = "Good evening, it's currently"
-    } else {
-        greetings.innerText = "Good morning, it's currently"
-    }
-}, 60000); */
