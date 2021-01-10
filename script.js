9591b8ae235dd7fefa13af9d748dcd0d48d3e937
Albert Asaftei <albert.asaftei00@gmail.com>
2021-01-10 17:17:49 +0100

    timezoneAPI connection + html data update

--- script.js
+++ script.js
@@ -1,14 +1,59 @@
-const endpoint = "https://timezoneapi.io/api/ip/?token=aNsiQLfUDzdnhRxsLmJP"
+const timezoneAPI = "https://timezoneapi.io/api/ip/?token=aNsiQLfUDzdnhRxsLmJP";
+const quotesAPI = "https://quotes.rest/yaml/theysaidso.quotes.openapi.yaml?v1.1/qod"
+const time = document.getElementById("time");
+const timezone = document.getElementById("timezone");
+const position = document.getElementById("position");
+const greetings = document.getElementById("greetings");
+let url = 
 
-fetch(endpoint).then(response => {
+fetch(timezoneAPI).then((response) => {
     return response.json();
+}).then((data) => {
+    position.innerText = `in ${data.data.timezone.capital}, ${data.data.timezone.country_code}`
+}).catch((e) => {
+    console.error(e);
+});
+
+fetch(quotesAPI).then(response => {
+    return response.json
 }).then(data => {
-    
-})
+    console.log(data)
+}).catch(e => {
+    console.error(e)
+});
+
+(function showDate() {
+    const date = new Date();
+    if (date.getHours() > 12 && date.getHours() < 18) {
+        greetings.innerText = "Good afternoon, it's currently"
+    } else if (date.getHours() >= 18) {
+        greetings.innerText = "Good evening, it's currently"
+        document.body.style.backgroundImage = "url(img/night.jpg)"
+    } else {
+        greetings.innerText = "Good morning, it's currently"
+    }
+
+    let hours = date.getHours()
+    let minutes = ''
+    if (date.getMinutes() < 10) {
+        minutes = `0${date.getMinutes()}`
+    } else {
+        minutes = date.getMinutes()
+    }
+    time.innerText = `${hours}:${minutes}`
+
+    setTimeout(showDate, 1000);
+})();
 
 
-(function showDate(){
+/* setInterval(function showDate() {
     const date = new Date();
     console.log(date.getHours(), date.getMinutes(), date.getSeconds())
-    setTimeout(showDate, 60000);
-})();
+    if (date.getHours() > 12 && date.getHours() < 18) {
+        greetings.innerText = "Good afternoon, it's currently"
+    } else if (date.getHours() > 18 && date.getHours()) {
+        greetings.innerText = "Good evening, it's currently"
+    } else {
+        greetings.innerText = "Good morning, it's currently"
+    }
+}, 60000); */
